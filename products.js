563d084c83b4ae2682c893b46768f6733f7ba47e
arvinteldho <arvinteldho@gmail.com>
2023-01-11 11:31:10 +0530

    john

---
+++ products.js
@@ -0,0 +1,36 @@
+export const products = [
+
+    {
+        id: 1,
+        name: 'Lolipop 1',
+        price: 10,
+        image:require('./assets/r1.jpg')
+
+    },
+    {
+        id: 2,
+        name: 'Lolipop 2',
+        price: 20,
+        image:require('./assets/r2.jpg')
+    },
+    {
+        id: 3,
+        name: 'Lolipop 3',
+        price: 20,
+        image:require('./assets/r3.jpg')
+    },
+    {
+        id: 4,
+        name: 'Lolipop 4',
+        price: 30,
+        image:require('./assets/r4.jpg')
+ 
+    },
+    {
+        id: 5,
+        name: 'Lolipop 5',
+        price: 50,
+        image:require('./assets/r5.jpg')
+    },
+
+]
